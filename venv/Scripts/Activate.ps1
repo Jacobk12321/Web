e514983fd9ca9ec420f67f155eb13f004b90299a
Jacobk12321 <knappj@cardiff.ac.uk>
2023-04-12 15:17:26 +0100

    got working now must use 3.4 in venv

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\knapj\oreilly-intro-to-flask-video\venv"
+$env:VIRTUAL_ENV="C:\Users\knapj\oreilly-intro-to-flask-video\Web-Apps\Web\venv"
 
 # Set the prompt to include the env name
 # Make sure _OLD_VIRTUAL_PROMPT is global
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\knapj\oreilly-intro-to-flask-video\venv"
+set "VIRTUAL_ENV=C:\Users\knapj\oreilly-intro-to-flask-video\Web-Apps\Web\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
