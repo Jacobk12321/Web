54ca47754f3ae31bd342931efa7a37298c9660d2
Jacobk12321 <knappj@cardiff.ac.uk>
2023-04-27 16:28:56 +0100

    error pages added

--- Website/main.py
+++ Website/main.py
@@ -100,6 +100,18 @@ def register():
         msg = 'Please fill out the form !'
     return render_template('register.html', msg = msg)
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('errors/500.html'), 500
+
+
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True)
